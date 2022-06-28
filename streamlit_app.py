4c2c76e426626eb0792c4991f59519691261b2b8
Anuj Soni <107437149+anujsoni95@users.noreply.github.com>
2022-06-28 15:07:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,7 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+
 # Display the list on the page 
 #streamlit.dataframe(fruits_to_show)fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
