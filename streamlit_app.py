b2cc5d4e41b4f53bc076f5aa5af9758850cd7774
Anuj Soni <107437149+anujsoni95@users.noreply.github.com>
2022-06-28 16:50:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 def insert_row_snowflake(new_fruit): 
            with my_cnx.cursor() as my_cur: 
-                my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('"+ papaya + "')")
+                my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + papaya + "')")
                 return "Thanks for adding" + new_fruit
            
 # Display the list on the page 
