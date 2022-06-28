dcd8da4ddb7041029a97858e85c98e3cbd16e017
Anuj Soni <107437149+anujsoni95@users.noreply.github.com>
2022-06-28 14:49:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,14 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Lets put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick from fruits : ", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 # Display the list on the page 
 #streamlit.dataframe(fruits_to_show)fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
